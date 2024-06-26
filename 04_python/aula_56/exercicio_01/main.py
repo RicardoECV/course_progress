dcf99a012814f956b5925438dc0925e6430d700b
RicardoECV <ricardo.c.valente@hotmail.com>
2024-06-26 21:36:36 +0100

    26/06/2024

---
+++ 04_python/aula_56/exercicio_01/main.py
@@ -0,0 +1,13 @@
+print('\n')
+print('=== Dados ===')
+print('Nome: (Ricardo Valente)')
+print('Morada: (Sesimbra)')
+print('Idade: (33)')
+print('\n')
+
+x = 10
+
+while x > 0: 
+    print(x)   
+    x = x-1
+    
---
+++ 04_python/aula_56/exercicio_02/main.py
@@ -0,0 +1,20 @@
+print('\n')
+
+x = 0
+nome_string = "Ricardo"
+z = 22
+
+def contagem(x):
+    while x > 0: 
+        if x % 2:
+            print(x)   
+            x = x-1
+        else:
+            x = x-1
+
+contagem(20)
+print("Nome:" + nome_string)
+
+
+print('\n')
+
